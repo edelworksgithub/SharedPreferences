020b1f76bcda6c3d3de56af8a549b8510f0313e4
edelworksgithub <info@edelworks.com>
2014-09-03 22:25:28 +0530

    Update sharedpreferences.js

--- www/sharedpreferences.js
+++ www/sharedpreferences.js
@@ -4,6 +4,36 @@ var exec = require('cordova/exec');
 var sharedpreferences = {
 	getSharedPreferences : function(file, mode, successCallback, errorCallback){
 		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'getSharedPreferences', [file, mode])
+	},
+	putString: function(key, string, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'putString', [key, string])
+	},
+	getString: function(key, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'getString', [key])
+	},
+	putBoolean: function(key, value, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'putBoolean', [key, value])
+	},
+	getBoolean: function(key, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'getBoolean', [key])
+	},
+	putInt: function(key, value, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'putInt', [key, value])
+	},
+	getInt: function(key, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'getInt', [key])
+	},
+	putLong: function(key, value, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'putLong', [key, value])
+	},
+	getLong: function(key, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'getLong', [key])
+	},
+	remove: function(key, successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'remove', [key])
+	},
+	clear: function(successCallback, errorCallback){
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'clear', ["null"])
 	}
 }
 
