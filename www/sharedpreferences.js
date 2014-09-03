adc71fa38b814f10d9363e47554dbe951c6fb683
edelworksgithub <info@edelworks.com>
2014-09-03 15:25:55 +0530

    Update sharedpreferences.js

--- www/sharedpreferences.js
+++ www/sharedpreferences.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 
 var sharedpreferences = {
 	getSharedPreferences : function(file, mode, successCallback, errorCallback){
-		cordova.exec(successCallback, errorCallback, 'SharedPreferences', 'getSharedPreferences', [file, mode])
+		cordova.exec(successCallback, errorCallback, 'Sharedpreferences', 'getSharedPreferences', [file, mode])
 	}
 }
 
